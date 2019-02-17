0a023d59eb47ef70d39ba17b6eb8fdd2f6e31674
Josef Kříž <pepakriz@gmail.com>
2019-02-17 19:28:43 +0100

    Implemented extension for `ReflectionMethod` constructor (fixed #68)

--- tests/src/Rules/data/throws-php-internal-functions.php
+++ tests/src/Rules/data/throws-php-internal-functions.php
@@ -6,6 +6,7 @@ use DateTime;
 use DateTimeImmutable;
 use ReflectionClass;
 use ReflectionFunction;
+use ReflectionMethod;
 use ReflectionObject;
 use ReflectionProperty;
 use ReflectionZendExtension;
@@ -19,6 +20,14 @@ class ValueObject
 	private $property;
 
 	private $secondProperty;
+
+	public function testReflection(): void
+	{
+	}
+
+	public function testDateTime(): void
+	{
+	}
 
 }
 
@@ -43,6 +52,12 @@ class Example
 		new ReflectionProperty('undefinedClass', 'property'); // error: Missing @throws ReflectionException annotation
 		new ReflectionProperty('undefinedClass', 'undefinedProperty'); // error: Missing @throws ReflectionException annotation
 
+		new ReflectionMethod(self::class, 'testReflection');
+		new ReflectionMethod(static::class, 'testReflection');
+		new ReflectionMethod(self::class, 'undefinedMethod'); // error: Missing @throws ReflectionException annotation
+		new ReflectionMethod('undefinedClass', 'testReflection'); // error: Missing @throws ReflectionException annotation
+		new ReflectionMethod('undefinedClass', 'undefinedMethod'); // error: Missing @throws ReflectionException annotation
+
 		new ReflectionFunction('count');
 		new ReflectionFunction('undefinedFunction'); // error: Missing @throws ReflectionException annotation
 
@@ -58,6 +73,11 @@ class Example
 		new ReflectionProperty(rand(0, 1) === 0 ? static::class : ValueObject::class, rand(0, 1) === 0 ? 'property' : 'secondProperty');
 		new ReflectionProperty(rand(0, 1) === 0 ? self::class : null, rand(0, 1) === 0 ? 'property' : 'secondProperty'); // error: Missing @throws ReflectionException annotation
 		new ReflectionProperty(rand(0, 1) === 0 ? self::class : Throwable::class, rand(0, 1) === 0 ? 'property' : 'undefinedProperty'); // error: Missing @throws ReflectionException annotation
+
+		new ReflectionMethod(rand(0, 1) === 0 ? self::class : ValueObject::class, rand(0, 1) === 0 ? 'testReflection' : 'testDateTime');
+		new ReflectionMethod(rand(0, 1) === 0 ? static::class : ValueObject::class, rand(0, 1) === 0 ? 'testReflection' : 'testDateTime');
+		new ReflectionMethod(rand(0, 1) === 0 ? self::class : null, rand(0, 1) === 0 ? 'testReflection' : 'testDateTime'); // error: Missing @throws ReflectionException annotation
+		new ReflectionMethod(rand(0, 1) === 0 ? self::class : Throwable::class, rand(0, 1) === 0 ? 'testReflection' : 'undefinedMethod'); // error: Missing @throws ReflectionException annotation
 
 		new ReflectionFunction(rand(0, 1) === 0 ? 'count' : 'sort');
 		new ReflectionFunction(rand(0, 1) === 0 ? 'count' : 'undefinedFunction'); // error: Missing @throws ReflectionException annotation
